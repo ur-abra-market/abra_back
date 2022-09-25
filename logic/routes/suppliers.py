1968c9be8d9387f2a11eb09135549a27c9b60b2f
Artem <ushakov.artem256@mail.ru>
2022-09-25 11:56:17 +0300

    delete method

--- logic/routes/suppliers.py
+++ logic/routes/suppliers.py
@@ -337,7 +337,7 @@ async def get_supplier_products(Authorize: AuthJWT = Depends(),
     )
 
 
-@suppliers.post("/delete_products/",
+@suppliers.delete("/delete_products/",
     summary="WORKS: Delete products (change is_active to 0).",
     response_model=ProductIdOut)
 async def get_supplier_products(products: List[int],
