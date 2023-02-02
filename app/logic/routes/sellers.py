294ca38542e5973db2a54defda73fdf4388559e6
Andrew Veselov <vesell4123@mail.ru>
2023-02-02 14:50:59 +0300

    create crud for seller address

--- app/logic/routes/sellers.py
+++ app/logic/routes/sellers.py
@@ -1,10 +1,11 @@
 from app.database import get_session
 from app.database.models import *
 from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.encoders import jsonable_encoder
 from fastapi_jwt_auth import AuthJWT
 from app.logic.consts import *
 from app.logic.queries import *
-from sqlalchemy import select, update
+from sqlalchemy import select, update, insert, delete
 from sqlalchemy.ext.asyncio import AsyncSession
 from fastapi.responses import JSONResponse
 from typing import List, Optional
@@ -29,14 +30,14 @@ class SellerUserNotification(BaseModel):
     on_account_support: bool
 
 
-class SellerUserAdress(BaseModel):
-    country: str
-    area: str
-    city: str
-    street: str
-    building: str
-    appartment: str
-    postal_code: str
+class SellerUserAddress(BaseModel):
+    country: str = None
+    area: str = None
+    city: str = None
+    street: str = None
+    building: str = None
+    appartment: str = None
+    postal_code: str = None
 
 
 class OrdersList(BaseModel):
@@ -52,12 +53,12 @@ sellers = APIRouter()
 
 @sellers.get(
     "/get_seller_info/",
-    summary="WORKS: returns dict with profile info,"\
-        "adresses, notifications, photo information"
+    summary="WORKS: returns dict with profile info," \
+            "adresses, notifications, photo information"
 )
 async def get_seller_info(
-    Authorize: AuthJWT = Depends(),
-    session: AsyncSession = Depends(get_session),
+        Authorize: AuthJWT = Depends(),
+        session: AsyncSession = Depends(get_session),
 ):
     Authorize.jwt_required()
     user_email = json.loads(Authorize.get_jwt_subject())["email"]
@@ -69,7 +70,7 @@ async def get_seller_info(
         )
     )
     user_query = user_query.fetchone()
-    user_profile_info = dict(user_query)\
+    user_profile_info = dict(user_query) \
         if user_query else {}
 
     notifications_query = await session.execute(
@@ -98,15 +99,15 @@ async def get_seller_info(
         ).where(UserAdress.user_id.__eq__(user_id))
     )
     user_adresses_query = user_adresses_query.fetchall()
-    user_adresses = dict(user_adresses_query)\
+    user_adresses = dict(user_adresses_query) \
         if user_adresses_query else {}
 
     profile_image_query = await session.execute(
-        select(UserImage.thumbnail_url, UserImage.source_url)\
+        select(UserImage.thumbnail_url, UserImage.source_url) \
             .where(UserImage.user_id.__eq__(user_id))
     )
     profile_image_query = profile_image_query.fetchall()
-    profile_image = dict(profile_image_query)\
+    profile_image = dict(profile_image_query) \
         if profile_image_query else {}
 
     result = dict(
@@ -126,9 +127,9 @@ async def get_seller_info(
     summary="Not working yet"
 )
 async def get_order_status(
-    status: Optional[int] = None,
-    # Authorize: AuthJWT = Depends(),
-    session: AsyncSession = Depends(get_session)
+        status: Optional[int] = None,
+        # Authorize: AuthJWT = Depends(),
+        session: AsyncSession = Depends(get_session)
 ):
     # Authorize.jwt_required()
     # user_id = await User.get_user_id(email=user_email)
@@ -145,44 +146,159 @@ async def get_order_status(
     summary="WORKS: update seller data, full adress is required, notifications - 1 route for all norificatios"
 )
 async def send_seller_data_info(
-    seller_data: SellerUserData = None,
-    seller_adress_data: SellerUserAdress = None,
-    seller_notifications_data: SellerUserNotification = None,
-    Authorize: AuthJWT = Depends(),
-    session: AsyncSession = Depends(get_session)
+        seller_data: SellerUserData = None,
+        seller_address_data: SellerUserAddress = None,
+        seller_notifications_data: SellerUserNotification = None,
+        Authorize: AuthJWT = Depends(),
+        session: AsyncSession = Depends(get_session)
 ):
     Authorize.jwt_required()
     user_email = json.loads(Authorize.get_jwt_subject())["email"]
     user_id = await User.get_user_id(email=user_email)
 
-    user_data: Optional[dict] =\
-        {key: value for key, value in dict(seller_data).items() if value}\
+    user_data: Optional[dict] = \
+        {key: value for key, value in dict(seller_data).items() if value} \
             if seller_data else None
-    adress_data: Optional[dict] =\
-        {key: value for key, value in dict(seller_adress_data).items() if value}\
-            if seller_adress_data else None
-    notifications_data: Optional[dict] =\
-        {key: value for key, value in dict(seller_notifications_data).items()}\
+    address_data: Optional[dict] = \
+        {key: value for key, value in dict(seller_address_data).items() if value} \
+            if seller_address_data else None
+    notifications_data: Optional[dict] = \
+        {key: value for key, value in dict(seller_notifications_data).items()} \
             if seller_notifications_data else None
-    
+
     if user_data:
         await session.execute(
             update(User).where(User.id.__eq__(user_id)).values(**(user_data))
         )
-    if adress_data:
+    if address_data:
         await session.execute(
-            update(UserAdress)\
-                .where(UserAdress.user_id.__eq__(user_id))\
-                    .values(**(adress_data))
+            update(UserAdress) \
+                .where(UserAdress.user_id.__eq__(user_id)) \
+                .values(**address_data)
         )
     if notifications_data:
         await session.execute(
-            update(UserNotification)\
-                .where(UserNotification.user_id.__eq__(user_id))\
-                    .values(**(notifications_data))
+            update(UserNotification) \
+                .where(UserNotification.user_id.__eq__(user_id)) \
+                .values(**notifications_data)
         )
+    await session.commit()
+    return JSONResponse(
+        status_code=status.HTTP_200_OK, content={"result": "DATA_HAS_BEEN_SENT"}
+    )
+
+
+@sellers.post('/add_address/')
+async def add_seller_address(
+        seller_address_data: SellerUserAddress,
+        Authorize: AuthJWT = Depends(),
+        session: AsyncSession = Depends(get_session)
+):
+    Authorize.jwt_required()
+    user_email = json.loads(Authorize.get_jwt_subject())["email"]
+    seller_id = await Seller.get_seller_id_by_email(user_email)
+    if seller_id is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail='USER_NOT_SELLER'
+        )
+
+    is_address = (await session.execute(
+        select(UserAdress)
+        .where(UserAdress.user_id.__eq__(seller_id))
+    )).first()
+
+    if is_address:
+        raise HTTPException(
+            status_code=status.HTTP_405_METHOD_NOT_ALLOWED,
+            detail='ADDRESS_ALREADY_ADDED'
+        )
+    seller_address_data = dict(seller_address_data)
+    seller_address_data['user_id'] = seller_id
+    await session.execute(insert(UserAdress).values(seller_address_data))
+    await session.commit()
+    return JSONResponse(
+        status_code=status.HTTP_201_CREATED,
+        content={'result': 'ADDRESS_SUCCESSFULLY_ADDED'}
+    )
+
+
+@sellers.patch('/update_address/', response_model=SellerUserAddress, response_model_exclude_unset=True)
+async def change_seller_address(
+        seller_address_data: SellerUserAddress,
+        Authorize: AuthJWT = Depends(),
+        session: AsyncSession = Depends(get_session)
+):
+    Authorize.jwt_required()
+    user_email = json.loads(Authorize.get_jwt_subject())["email"]
+    seller_id = await Seller.get_seller_id_by_email(user_email)
+    if seller_id is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail='USER_NOT_SELLER'
+        )
+    seller_address_data = {key: value for key, value in dict(seller_address_data).items() if value is not None}
+    await session.execute(
+        update(UserAdress)
+        .values(dict(seller_address_data))
+        .where(UserAdress.user_id.__eq__(seller_id))
+    )
     await session.commit()
 
     return JSONResponse(
-        status_code=status.HTTP_200_OK, content={"result": "DATA_HAS_BEEN_SENT"}
+        status_code=status.HTTP_200_OK,
+        content={'result': 'ADDRESS_WAS_UPDATED'}
+    )
+
+
+@sellers.get('/get_address/', response_model=SellerUserAddress)
+async def get_seller_address(
+        Authorize: AuthJWT = Depends(),
+        session: AsyncSession = Depends(get_session)
+):
+    Authorize.jwt_required()
+    user_email = json.loads(Authorize.get_jwt_subject())["email"]
+    seller_id = await Seller.get_seller_id_by_email(user_email)
+    if seller_id is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail='USER_NOT_SELLER'
+        )
+
+    seller_address = (await session.execute(
+        select(UserAdress.country,
+               UserAdress.area,
+               UserAdress.city,
+               UserAdress.street,
+               UserAdress.building,
+               UserAdress.appartment,
+               UserAdress.postal_code)
+        .where(UserAdress.user_id.__eq__(seller_id))
+    )).first()
+    seller_address = jsonable_encoder(seller_address)
+    return JSONResponse(
+        status_code=status.HTTP_200_OK,
+        content={'result': seller_address}
+    )
+
+
+@sellers.delete('/remove_address/')
+async def remove_seller_address(
+        Authorize: AuthJWT = Depends(),
+        session: AsyncSession = Depends(get_session)
+):
+    Authorize.jwt_required()
+    user_email = json.loads(Authorize.get_jwt_subject())["email"]
+    seller_id = await Seller.get_seller_id_by_email(user_email)
+    if seller_id is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail='USER_NOT_SELLER'
+        )
+
+    await session.execute(delete(UserAdress).where(UserAdress.user_id.__eq__(seller_id)))
+    await session.commit()
+    return JSONResponse(
+        status_code=status.HTTP_200_OK,
+        content='ADDRESS_WAS_DELETED'
     )
