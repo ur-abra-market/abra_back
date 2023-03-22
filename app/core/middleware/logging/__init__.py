5db2f039cfba8378f0467145df518add2747b465
kbondar17 <aa@bb.mail.ru>
2023-03-22 17:18:06 +0300

    fix imports

---
+++ app/core/middleware/logging/__init__.py
@@ -0,0 +1,4 @@
+from .init_custom_logger import init_custom_logging
+from .request_logger import set_request_logging_middleware
+
+__all__ = ("init_custom_logging", "set_request_logging_middleware")
