1a4363a71ad13b1d7c648c2dabe91c34619f7272
toymaj <sermed512@gmail.com>
2023-03-22 17:34:52 +0300

    Remove

--- app/api/api.py
+++ app/api/api.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from .routers import categories_router, password_router, register_router, test_router
+from .routers import categories_router, password_router, register_router
 
 
 def create_api_router() -> APIRouter:
@@ -8,7 +8,6 @@ def create_api_router() -> APIRouter:
     api_router.include_router(categories_router)
     api_router.include_router(password_router)
     api_router.include_router(register_router)
-    api_router.include_router(test_router)
 
     return api_router
 
