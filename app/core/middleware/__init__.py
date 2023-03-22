1c95e9353f720b1e5f3e836d0cbdda1b979b1dc5
kbondar17 <aa@bb.mail.ru>
2023-03-22 17:26:05 +0300

    set middleware

--- app/core/middleware/__init__.py
+++ app/core/middleware/__init__.py
@@ -1,8 +1,7 @@
 from fastapi import FastAPI
 
 from .cors import setup_cors_middleware
-from .logging import set_up_logging_middleware
-from .init_custom_logger import init_custom_logging
+from .logging import set_request_logging_middleware, init_custom_logging
 from core.settings import logging_settings
 
 __all__ = ("setup",)
@@ -12,4 +11,4 @@ def setup(app: FastAPI) -> None:
     setup_cors_middleware(app=app)
     if logging_settings.CUSTOM_LOGGING_ON:
         init_custom_logging()
-        set_up_logging_middleware(app=app)
+        set_request_logging_middleware(app=app)
