b5f30d0a105108f4704bef485d5cdf971bd62547
toymaj <sermed512@gmail.com>
2023-03-27 17:34:44 +0300

    Include all routers

--- app/api/api.py
+++ app/api/api.py
@@ -5,9 +5,11 @@ from .routers import (
     login_router,
     logout_router,
     password_router,
+    products_router,
     register_router,
     reviews_router,
     sellers_router,
+    suppliers_router,
     users_router,
 )
 
@@ -18,9 +20,11 @@ def create_api_router() -> APIRouter:
     api_router.include_router(login_router, tags=["login"], prefix="/login")
     api_router.include_router(logout_router, tags=["logout"], prefix="/logout")
     api_router.include_router(password_router, tags=["password"], prefix="/password")
+    api_router.include_router(products_router, tags=["products"], prefix="/products")
     api_router.include_router(register_router, tags=["register"], prefix="/register")
     api_router.include_router(reviews_router, tags=["reviews"], prefix="/reviews")
     api_router.include_router(sellers_router, tags=["sellers"], prefix="/sellers")
+    api_router.include_router(suppliers_router, tags=["suppliers"], prefix="/suppliers")
     api_router.include_router(users_router, tags=["users"], prefix="/users")
 
     return api_router
--- app/api/routers/__init__.py
+++ app/api/routers/__init__.py
@@ -2,9 +2,11 @@ from .categories import router as categories_router
 from .login import router as login_router
 from .logout import router as logout_router
 from .password import router as password_router
+from .products import router as products_router
 from .register import router as register_router
 from .reviews import router as reviews_router
 from .sellers import router as sellers_router
+from .suppliers import router as suppliers_router
 from .users import router as users_router
 
 __all__ = (
@@ -12,8 +14,10 @@ __all__ = (
     "login_router",
     "logout_router",
     "password_router",
+    "products_router",
     "register_router",
     "reviews_router",
     "sellers_router",
+    "suppliers_router",
     "users_router",
 )
