46cc2e38cb64fe7f2980547ec4f575c0fa80a669
fouinyla <mefouiny@gmail.com>
2023-04-01 23:33:19 +0300

    edit docker-compose configs
    edit prebuild script for aws eb

--- .platform/hooks/prebuild/build.sh
+++ .platform/hooks/prebuild/build.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-mv docker-compose.prod.yml docker-compose.yml
+cat docker-compose.yml docker-compose.alembic.yml > docker-compose.new.yml
+mv docker-compose.new.yml docker-compose.yml
+rm docker-compose.alembic.yml docker-compose.db.yml
---
+++ start.sh
@@ -0,0 +1,1 @@
+docker compose -f docker-compose.yml -f docker-compose.alembic.yml -f docker-compose.db.yml up --build
