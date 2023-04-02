ef5a4daa22832eb7bd4f94727e5ba92096d7f0c9
fouinyla <mefouiny@gmail.com>
2023-04-02 15:34:18 +0300

    fix prebuild script for aws eb

--- .platform/hooks/prebuild/build.sh
+++ .platform/hooks/prebuild/build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-cat docker-compose.yml docker-compose.alembic.yml > docker-compose.new.yml
+cat docker-compose.yml > docker-compose.new.yml
+echo -e "\n" >> docker-compose.new.yml
+tail --lines=+4 docker-compose.alembic.yml >> docker-compose.new.yml
 mv docker-compose.new.yml docker-compose.yml
 rm docker-compose.alembic.yml docker-compose.db.yml
