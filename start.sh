e2eca9c67632ba0e13c65cc9f18bb2f173875bbc
fouinyla <mefouiny@gmail.com>
2023-04-02 15:51:00 +0300

    rename docker compose configs; create config for prod application

--- start.sh
+++ start.sh
@@ -1,1 +1,1 @@
-docker compose -f docker-compose.yml -f docker-compose.alembic.yml -f docker-compose.db.yml up --build
+docker compose -f docker-compose.app.yml -f docker-compose.alembic.yml -f docker-compose.db.yml up --build
