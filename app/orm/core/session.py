20b5fc6c109011a0f54642b8efbd642ffc2bcfea
toymaj <sermed512@gmail.com>
2023-04-07 22:10:35 +0300

    Add echo

--- app/orm/core/session.py
+++ app/orm/core/session.py
@@ -5,12 +5,11 @@ from core.settings import database_settings
 
 POOL_RECYCLE = 60 * 5  # 300
 
-_engine = create_async_engine(database_settings.url, pool_recycle=POOL_RECYCLE)
+_engine = create_async_engine(database_settings.url, pool_recycle=POOL_RECYCLE, echo=True)
 async_sessionmaker = sessionmaker(
     bind=_engine,
     class_=AsyncSession,
     expire_on_commit=False,
     autocommit=False,
     autoflush=False,
-    echo=True,
 )
