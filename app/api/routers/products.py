0a02e9ad4ea42a7fce82a0b32e10cf9c908b5966
toymaj <sermed512@gmail.com>
2023-04-16 21:32:34 +0300

    Add new endpoint in products

--- app/api/routers/products.py
+++ app/api/routers/products.py
@@ -3,15 +3,16 @@
 from typing import List
 
 from fastapi import APIRouter
-from fastapi.param_functions import Depends, Path
-from sqlalchemy import func
+from fastapi.exceptions import HTTPException
+from fastapi.param_functions import Depends, Path, Query
+from sqlalchemy import and_, func
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.orm import joinedload, outerjoin
 from starlette import status
 
-from core.depends import get_session
+from core.depends import UserObjects, auth_required, get_session
 from core.orm import orm
-from orm import ProductModel, ProductReviewModel, SupplierModel
+from orm import ProductModel, ProductReviewModel, SellerFavoriteModel, SupplierModel
 from schemas import (
     ApplicationResponse,
     Product,
@@ -108,4 +109,104 @@ async def get_review_grades_info(
             "review_count": grade_info.reviews_count,
             "details": review_details,
         },
+    }
+
+
+# noinspection PyUnusedLocal
+@router.patch(
+    path="/favorite_product/",
+    description="Moved to POST /products/addFavorite, DELETE /products/removeFavorite",
+    deprecated=True,
+    status_code=status.HTTP_418_IM_A_TEAPOT,
+)
+async def favorite_product_deprecated(
+    product_id: int = Query(...),
+    is_favorite: bool = Query(...),
+) -> None:
+    raise HTTPException(
+        status_code=status.HTTP_418_IM_A_TEAPOT,
+        detail="Frontend, change paths быро",  # noqa
+    )
+
+
+async def add_favorite_core(product_id: int, seller_id: int, session: AsyncSession) -> None:
+    seller_favorite = await orm.sellers_favorites.get_one(
+        session=session,
+        where=[
+            and_(
+                SellerFavoriteModel.seller_id == seller_id,
+                SellerFavoriteModel.product_id == product_id,
+            )
+        ],
+    )
+    if seller_favorite:
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail="Already in favorites",
+        )
+
+    await orm.sellers_favorites.insert_one(
+        session=session,
+        values={
+            SellerFavoriteModel.seller_id: seller_id,
+            SellerFavoriteModel.product_id: product_id,
+        },
+    )
+
+
+@router.post(
+    path="/addFavorite/",
+    summary="WORKS: add product in favorite",
+    response_model=ApplicationResponse[bool],
+    status_code=status.HTTP_200_OK,
+)
+async def add_favorite(
+    product_id: int = Query(...),
+    user: UserObjects = Depends(auth_required),
+    session: AsyncSession = Depends(get_session),
+) -> ApplicationResponse[bool]:
+    if not user.orm.seller:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Seller not found")
+
+    await add_favorite_core(
+        seller_id=user.schema.seller.id, product_id=product_id, session=session
+    )
+
+    return {
+        "ok": True,
+        "result": True,
+    }
+
+
+async def remove_favorite_core(product_id: int, seller_id: int, session: AsyncSession) -> None:
+    await orm.sellers_favorites.delete_one(
+        session=session,
+        where=and_(
+            SellerFavoriteModel.seller_id == seller_id,
+            SellerFavoriteModel.product_id == product_id,
+        ),
+    )
+
+
+@router.delete(
+    path="/removeFavorite/",
+    summary="WORKS: remove product in favorite",
+    response_model=ApplicationResponse[bool],
+    status_code=status.HTTP_200_OK,
+)
+async def remove_favorite(
+    product_id: int = Query(...),
+    user: UserObjects = Depends(auth_required),
+    session: AsyncSession = Depends(get_session),
+) -> ApplicationResponse[bool]:
+    if not user.orm.seller:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Seller not found")
+
+    await remove_favorite_core(
+        seller_id=user.schema.seller.id, product_id=product_id, session=session
+    )
+
+    return {
+        "ok": True,
+        "result": True,
     }
