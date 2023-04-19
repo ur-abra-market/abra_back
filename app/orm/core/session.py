f0a0da0764ba41a68786c61a15c0eec0e4771ea5
toymaj <sermed512@gmail.com>
2023-04-19 16:58:08 +0300

    Remove unused code in session create

--- app/orm/core/session.py
+++ app/orm/core/session.py
@@ -1,17 +1,14 @@
 # mypy: disable-error-code="no-redef"
-import traceback
+from __future__ import annotations
+
+from typing import Final
 
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
 from sqlalchemy.orm import sessionmaker
 
-root_process = traceback.format_stack()[0]
-if "bin/alembic" in root_process:
-    from app.core.settings import database_settings, fastapi_settings
-else:
-    from core.settings import database_settings, fastapi_settings  # noqa
+from core.settings import database_settings, fastapi_settings  # noqa
 
-
-POOL_RECYCLE = 60 * 5  # 300
+POOL_RECYCLE: Final[int] = 60 * 5  # 300
 
 _engine = create_async_engine(
     database_settings.url,
