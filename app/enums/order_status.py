f1035d2e46bb9e82f10cbbc8725a83a7b373a383
toymaj <sermed512@gmail.com>
2023-04-19 17:02:54 +0300

    refactor names in enum

--- app/enums/order_status.py
+++ app/enums/order_status.py
@@ -5,8 +5,8 @@ class OrderStatus(Enum):
     unpaid = 1
     shipped = 2
     to_be_shipped = 3
-    to_be_reviwed = 4
+    to_be_reviewed = 4
     completed = 5
     paid = 6
-    awaiting_a_refund = 7
+    awaiting_refund = 7
     cancelled = 8
