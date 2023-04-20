e07be9ace31b2fce0a73c42c2593ae4d316ff2bd
kbondar17 <aa@bb.mail.ru>
2023-04-20 16:04:20 +0300

    fix models

--- app/orm/category_variation.py
+++ app/orm/category_variation.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+from sqlalchemy import UniqueConstraint
 from sqlalchemy.orm import Mapped
 
 from .core import ORMModel, category_variation_type_fk, mixins
@@ -7,3 +8,4 @@ from .core import ORMModel, category_variation_type_fk, mixins
 
 class CategoryVariationModel(mixins.CategoryIDMixin, ORMModel):
     variation_type_id: Mapped[category_variation_type_fk]
+    __table_args__ = (UniqueConstraint("category_id", "variation_type_id", name="_cat_var_uc"),)
