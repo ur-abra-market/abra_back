cefa29bfcd28ffb582cfd2c7a29f89017e48a845
kbondar17 <aa@bb.mail.ru>
2023-04-26 21:06:41 +0300

    fix schemas

--- app/schemas/orm/product_price.py
+++ app/schemas/orm/product_price.py
@@ -3,6 +3,8 @@ from __future__ import annotations
 import datetime as dt
 from typing import TYPE_CHECKING, Optional
 
+from pydantic import Field
+
 from .core import ORMSchema
 
 if TYPE_CHECKING:
@@ -10,9 +12,10 @@ if TYPE_CHECKING:
 
 
 class ProductPrice(ORMSchema):
+    id: int = Field(return_in_api=False)
     value: float
     discount: Optional[float] = None
     min_quantity: int
     start_date: dt.datetime
     end_date: Optional[dt.datetime] = None
-    product: Optional[Product] = None
+    product: Optional[Product] = Field(return_in_api=False)
