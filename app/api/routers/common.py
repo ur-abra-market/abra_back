8e1bf79afc52c1750e785c525ceaf7b36ea18388
kbondar17 <aa@bb.mail.ru>
2023-04-28 22:01:35 +0300

    add route

--- app/api/routers/common.py
+++ app/api/routers/common.py
@@ -8,7 +8,7 @@ from starlette import status
 from core.app import crud
 from core.depends import get_session
 from orm import CountryModel
-from schemas import ApplicationResponse, Country
+from schemas import ApplicationResponse, CompanyNumOfEmployees, Country
 from typing_ import RouteReturnT
 
 router = APIRouter()
@@ -30,4 +30,23 @@ async def get_all_country_codes(
     return {
         "ok": True,
         "result": await get_all_country_core(session=session),
+    }
+
+
+async def get_companies_employees_options_core(session: AsyncSession):
+    return await crud.company_num_of_employees.get.many(session=session)
+
+
+@router.get(
+    path="/NumOfEmployeesOptions/",
+    summary="WORKS: get options of company number of employees",
+    response_model=ApplicationResponse[List[CompanyNumOfEmployees]],
+    status_code=status.HTTP_200_OK,
+)
+async def get_companies_employees_options(
+    session: AsyncSession = Depends(get_session),
+) -> RouteReturnT:
+    return {
+        "ok": True,
+        "result": await get_companies_employees_options_core(session=session),
     }
