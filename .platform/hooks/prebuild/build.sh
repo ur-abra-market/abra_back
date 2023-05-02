d430409e2811b458211eb67e2df8d4b463b550fc
oregu <mefouiny@gmail.com>
2023-05-02 21:09:29 +0300

    edit prebuild script: add PROJECT_PATH export

--- .platform/hooks/prebuild/build.sh
+++ .platform/hooks/prebuild/build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+export PROJECT_PATH=$(pwd)
+
 head -n -2 ./docker/compose/app.prod.yml > docker-compose.yml
 tail --lines=+4 ./docker/compose/alembic.yml >> docker-compose.yml
 tail --lines=+4 ./docker/compose/population.dev.yml | head -n -2 ./docker/compose/population.dev.yml >> docker-compose.yml
