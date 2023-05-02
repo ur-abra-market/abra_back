440606c04530222fa5432d2c110f6adea84bec3f
fouinyla <mefouiny@gmail.com>
2023-05-02 15:37:53 +0300

    edit prebuild script

--- .platform/hooks/prebuild/build.sh
+++ .platform/hooks/prebuild/build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 head -n -2 docker-compose.app.prod.yml > docker-compose.yml
-echo -e "\n" >> docker-compose.yml
 tail --lines=+4 docker-compose.alembic.yml >> docker-compose.yml
+echo -e "\n" >> docker-compose.yml
 tail --lines=+2 docker-compose.app.prod.yml >> docker-compose.yml
 rm docker-compose.alembic.yml docker-compose.db.yml docker-compose.app.yml docker-compose.app.prod.yml
