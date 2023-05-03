f0e5489a645c0cfcdcc82a71908d4869911f6628
kbondar17 <aa@bb.mail.ru>
2023-05-03 18:51:41 +0300

    fix schema

--- app/schemas/orm/country.py
+++ app/schemas/orm/country.py
@@ -1,11 +1,9 @@
 from __future__ import annotations
 
-from typing import Optional
-
 from .core import ORMSchema
 
 
 class Country(ORMSchema):
     country: str
     country_code: str
-    flag: Optional[str]
+    flag: str
