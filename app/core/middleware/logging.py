42d32d1dfc2bc71471496c43a247d9c2a8996d70
toymaj <sermed512@gmail.com>
2023-05-04 23:43:55 +0300

    better logging middleware

--- app/core/middleware/logging.py
+++ app/core/middleware/logging.py
@@ -2,72 +2,29 @@ from __future__ import annotations
 
 import asyncio
 import uuid
-from typing import Any, Awaitable, Callable, Optional, Type
+from typing import Awaitable, Callable
 
 from fastapi import FastAPI
 from fastapi.requests import Request
-from fastapi.responses import JSONResponse
-from pydantic import UUID4, BaseModel
+from fastapi.responses import JSONResponse, Response
+from starlette import status
 from starlette.middleware.base import BaseHTTPMiddleware
 
 from logger import logger
-from typing_ import DictStrAny
 
-from ._responses import ERROR_RESPONSE
-
-
-class _ExceptionInfo(BaseModel):
-    type: Type[BaseException]
-    exception: Optional[str]
-
-
-class _RequestInfo(BaseModel):
-    method: str
-    url: str
-
-
-class _ResponseInfo(BaseModel):
-    duration: float
-    status_code: int
-
-
-class _RequestMetadata(BaseModel):
-    request_id: UUID4
-    request_info: _RequestInfo
-    response_info: _ResponseInfo
-    exception_info: Optional[_ExceptionInfo] = None
-
-
-def _request_metadata(
-    request: Request,
-    response: JSONResponse,
-    duration: float,
-    exception: Optional[Any] = None,
-) -> _RequestMetadata:
-    return _RequestMetadata(
-        request_id=uuid.uuid4(),
-        request_info=_RequestInfo(
-            method=request.method,
-            url=request.url._url,  # noqa
-        ),
-        response_info=_ResponseInfo(
-            duration=duration,
-            status_code=response.status_code,
-        ),
-        exception_info=_ExceptionInfo(
-            type=type(exception),
-            exception=str(exception),
-        )
-        if exception
-        else None,
-    )
+ERROR_RESPONSE = JSONResponse(
+    status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+    content={
+        "ok": False,
+        "error": "Unhandled error",
+        "error_code": status.HTTP_500_INTERNAL_SERVER_ERROR,
+    },
+)
 
 
 async def logging_middleware(
-    request: Request, call_next: Callable[[Request], Awaitable[JSONResponse]]
-) -> JSONResponse:
-    _kwargs: DictStrAny = {"request": request}
-
+    request: Request, call_next: Callable[[Request], Awaitable[Response]]
+) -> Response:
     loop = asyncio.get_event_loop()
     start_time = loop.time()
 
@@ -77,10 +34,18 @@ async def logging_middleware(
         logger.exception(exception)
 
         response = ERROR_RESPONSE
-        _kwargs.update(exception=exception)
 
-    _kwargs.update({"response": response, "duration": round(loop.time() - start_time, 5)})
-    logger.info("Request processing", extra=_request_metadata(**_kwargs).dict())
+    logger.info(
+        "Middleware - ID [%s] -  TIME [%ss] - CLIENT [%s][%s][%s] - RESPONSE [%s]"
+        % (
+            uuid.uuid4(),
+            round(loop.time() - start_time, 5),
+            request.method,
+            request.url,
+            request.client,
+            response.status_code,
+        )
+    )
 
     return response
 
