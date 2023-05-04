976e7304420da6beed9fd2d9c3e997981a234d94
Dmitry <99758072+DmitryBurov1911@users.noreply.github.com>
2023-05-04 19:20:45 +0300

    Update category_property_type.py

--- app/orm/category_property_type.py
+++ app/orm/category_property_type.py
@@ -14,24 +14,6 @@ if TYPE_CHECKING:
 class CategoryPropertyTypeModel(ORMModel):
     name: Mapped[str_30]
 
-    colors: Mapped[List] = [
-        "White",
-        "Beige",
-        "Sand",
-        "Gray",
-        "Black",
-        "Metallic",
-        "Bronze",
-        "Red",
-        "Orange",
-        "Yellow",
-        "Green",
-        "Blue",
-        "Indigo",
-        "Lilac",
-        "Purple",
-    ]
-
     category: Mapped[List[CategoryModel]] = relationship(
         secondary="category_property",
         back_populates="properties",
