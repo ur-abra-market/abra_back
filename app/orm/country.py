6354c3d5a787dcc8d87761ce6c6b369c41e52e82
toymaj <sermed512@gmail.com>
2023-05-05 15:51:04 +0300

    fix

--- app/orm/country.py
+++ app/orm/country.py
@@ -7,15 +7,13 @@ from sqlalchemy.orm import Mapped, mapped_column, relationship
 from .core import ORMModel, str_4, str_50, text
 
 if TYPE_CHECKING:
-    from enums import CurrencyEnum
-
     from .seller_address import SellerAddressModel
 
 
 class CountryModel(ORMModel):
     country: Mapped[str_50] = mapped_column(unique=True)
     country_code: Mapped[str_4]
-    currency: Mapped[CurrencyEnum]
+    currency: Mapped[str]
     flag: Mapped[text]
 
     addresses: Mapped[List[SellerAddressModel]] = relationship(back_populates="country")
