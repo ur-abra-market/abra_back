d3cbeb6b29d7ff6ca7cb5b0eae27e2d964109672
toymaj <sermed512@gmail.com>
2023-05-05 22:58:51 +0300

    fix fixtures namespace

--- tests/endpoints/suppliers/conftest.py
+++ tests/endpoints/suppliers/conftest.py
@@ -1,12 +1,7 @@
-from typing import Any, Dict, Final, List
+from typing import Final, List
 
 import pytest
-from httpx import AsyncClient
-from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy.orm import joinedload
 
-from core.app import crud
-from orm import SupplierModel, UserModel
 from typing_ import DictStrAny
 
 CATEGORY_ID: Final[int] = 1
@@ -15,7 +10,7 @@ VARIATIONS: Final[List[int]] = [3, 4]
 
 
 @pytest.fixture
-def add_product_request() -> Dict[str, Any]:
+def add_product_request() -> DictStrAny:
     return {
         "name": "Test Product",
         "description": "This is a test product",
@@ -30,27 +25,3 @@ def add_product_request() -> Dict[str, Any]:
             },
         ],
     }
-
-
-@pytest.fixture(scope="session")
-def _register_url() -> str:
-    return "/register/supplier/"
-
-
-@pytest.fixture(scope="session")
-def supplier_json() -> DictStrAny:
-    return {"email": "rick@example.com", "password": "RickPassword1!"}
-
-
-@pytest.fixture(autouse=True, scope="session")
-async def register_supplier(client: AsyncClient, _register_url: str, supplier_json: DictStrAny):
-    await client.post(url=_register_url, json=supplier_json)
-
-
-@pytest.fixture(scope="session")
-async def pure_supplier(session: AsyncSession, supplier_json: DictStrAny) -> UserModel:
-    return await crud.users.get.one(
-        session=session,
-        where=[UserModel.email == supplier_json["email"]],
-        options=[joinedload(UserModel.supplier).joinedload(SupplierModel.company)],
-    )
