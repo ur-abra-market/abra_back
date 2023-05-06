daedcc8b53ebe70e4eadd0555909b734040e80dd
lexsorokin <lex.sorokin.dev@gmail.com>
2023-05-06 14:11:40 +0300

    add Optional to last_name field

--- app/schemas/requests/bodies/user_data.py
+++ app/schemas/requests/bodies/user_data.py
@@ -1,10 +1,12 @@
 from __future__ import annotations
 
+from typing import Optional
+
 from ...schema import ApplicationSchema
 
 
 class UserData(ApplicationSchema):
     first_name: str
-    last_name: str
+    last_name: Optional[str] = None
     phone_country_code: str
     phone_number: str
