566439470bdae0f7bb9b4b275c575e6ec84e03f6
lexsorokin <lex.sorokin.dev@gmail.com>
2023-05-08 19:22:28 +0300

    changed last_name to required  in UserData request schema

--- app/schemas/requests/bodies/user_data.py
+++ app/schemas/requests/bodies/user_data.py
@@ -1,12 +1,10 @@
 from __future__ import annotations
 
-from typing import Optional
-
 from ...schema import ApplicationSchema
 
 
 class UserData(ApplicationSchema):
     first_name: str
-    last_name: Optional[str] = None
+    last_name: str
     phone_country_code: str
     phone_number: str
