55994ffb0bde7b0dc06f491baa5fc9f06dc9d3cb
lexsorokin <lex.sorokin.dev@gmail.com>
2023-06-11 19:15:47 +0300

    add phone mixin to user update schema

--- app/schemas/requests/bodies/user_data_update.py
+++ app/schemas/requests/bodies/user_data_update.py
@@ -2,9 +2,10 @@ from __future__ import annotations
 
 from typing import Optional
 
+from ...mixins import PhoneNumber
 from ...schema import ApplicationSchema
 
 
-class UserDataUpdate(ApplicationSchema):
+class UserDataUpdate(PhoneNumber, ApplicationSchema):
     first_name: Optional[str] = None
     last_name: Optional[str] = None
