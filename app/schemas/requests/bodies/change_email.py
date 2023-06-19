0db9c67aa9c73dd303c17bba226a2c443f6a0999
lexsorokin <lex.sorokin.dev@gmail.com>
2023-06-19 12:49:33 +0300

    fix email validation

--- app/schemas/requests/bodies/change_email.py
+++ app/schemas/requests/bodies/change_email.py
@@ -8,11 +8,11 @@ from ...schema import ApplicationSchema
 
 
 class ChangeEmail(ApplicationSchema):
-    new_email: EmailStr
     confirm_email: EmailStr
+    new_email: EmailStr
 
     @validator("new_email")
     def emails_are_same(cls, v: str, values: DictStrAny) -> str:
-        if v != values["confirm_email"]:
+        if v != values.get("confirm_email"):
             raise ValueError("emails not same")
         return v
