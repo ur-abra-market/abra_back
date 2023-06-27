327d9de1858f81daeb37661a87cd5c8e64b2658f
lexsorokin <lex.sorokin.dev@gmail.com>
2023-06-27 18:53:37 +0300

    refactor add & update address endpoints

--- app/orm/seller_address.py
+++ app/orm/seller_address.py
@@ -9,9 +9,10 @@ from .core import ORMModel, bool_false, mixins, str_20, str_50, str_100
 if TYPE_CHECKING:
     from .country import CountryModel
     from .seller import SellerModel
+    from .seller_address_phone import SellerAddressPhoneModel
 
 
-class SellerAddressModel(mixins.SellerIDMixin, mixins.NameMixin, mixins.PhoneMixin, ORMModel):
+class SellerAddressModel(mixins.SellerIDMixin, mixins.NameMixin, mixins.CountryIDMixin, ORMModel):
     is_main: Mapped[bool_false]
 
     area: Mapped[Optional[str_50]]
@@ -23,3 +24,6 @@ class SellerAddressModel(mixins.SellerIDMixin, mixins.NameMixin, mixins.PhoneMix
 
     country: Mapped[Optional[CountryModel]] = relationship(back_populates="addresses")
     seller: Mapped[Optional[SellerModel]] = relationship(back_populates="addresses")
+    phone: Mapped[Optional[SellerAddressPhoneModel]] = relationship(
+        back_populates="seller_address"
+    )
---
+++ app/schemas/orm/seller_address_phone.py
@@ -0,0 +1,15 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING, Optional
+
+from .core import ORMSchema
+
+if TYPE_CHECKING:
+    from .country import Country
+    from .seller_address import SellerAddress
+
+
+class CompanyPhone(ORMSchema):
+    phone_number: Optional[str] = None
+    seller_address: Optional[SellerAddress] = None
+    country: Optional[Country] = None
