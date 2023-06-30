3a3808d7a49883c9ef57c3059d86a21701d41f31
Dmitry <kiberl354@gmail.com>
2023-06-30 19:36:14 +0300

    fix business

--- app/api/routers/suppliers.py
+++ app/api/routers/suppliers.py
@@ -676,20 +676,20 @@ async def get_notifications(
 
 
 @router.get(
-    path="/hasCompanyInfo/",
+    path="/hasBusinessInfo/",
     summary="WORKS: get Company info",
     response_model=ApplicationResponse[bool],
     status_code=status.HTTP_200_OK,
 )
-def has_company_info(user: SupplierAuthorization) -> RouteReturnT:
+def has_business_info(user: SupplierAuthorization) -> RouteReturnT:
     return {"ok": True, "result": bool(user.supplier.company)}
 
 
 @router.get(
-    path="/hasBusinessInfo/",
+    path="/hasPersonalInfo/",
     summary="WORKS: get Personal info",
     response_model=ApplicationResponse[bool],
     status_code=status.HTTP_200_OK,
 )
-def has_business_info(user: SupplierAuthorization) -> RouteReturnT:
+def has_personal_info(user: SupplierAuthorization) -> RouteReturnT:
     return {"ok": True, "result": bool(user.first_name)}
