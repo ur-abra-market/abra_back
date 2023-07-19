025d91b63f59d0a59a2105d143f6fbacd632bd99
lexsorokin <lex.sorokin.dev@gmail.com>
2023-07-19 11:53:52 +0300

    edit product & restore product endpoints

---
+++ app/schemas/uploads/product_edit.py
@@ -0,0 +1,16 @@
+from __future__ import annotations
+
+from typing import List, Optional
+
+from ..schema import ApplicationSchema
+from .product_price import ProductPriceUpload
+
+
+class ProductEdit(ApplicationSchema):
+    name: Optional[str]
+    category_id: Optional[int]
+    properties: Optional[List[int]] = None
+    variations: Optional[List[int]] = None
+    description: Optional[str] = None
+    grade_average: Optional[float] = 0.0
+    prices: Optional[List[ProductPriceUpload]] = None
