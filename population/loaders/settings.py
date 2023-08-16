30e72d5da365bf1a7b9d01a0f691aa26a205383a
oregu <mefouiny@gmail.com>
2023-08-16 15:52:40 +0300

    add custom load for users and products: set count in .env

--- population/loaders/settings.py
+++ population/loaders/settings.py
@@ -7,12 +7,15 @@ class AdminSettings(BaseSettings):
     ADMIN_PASSWORD: str = "Password1Q!"
 
 
-class UsersSettings(BaseSettings):
+class PopulationSettings(BaseSettings):
     SUPPLIER_EMAIL_LOCAL: str = "supplier"
     SELLER_EMAIL_LOCAL: str = "seller"
     EMAIL_DOMAIN: str = "gmail.com"
     DEFAULT_PASSWORD: str = "Password1!"
+    SUPPLIERS_COUNT: int = 2
+    SELLERS_COUNT: int = 2
+    PRODUCTS_COUNT_RANGE: int = 20
 
 
 admin_settings = AdminSettings()
-user_settings = UsersSettings()
+population_settings = PopulationSettings()
