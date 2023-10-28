e93a0ef864ba869c28b929eb10a375820e5d5125
oleh <67467007+fouinyla@users.noreply.github.com>
2023-10-28 10:08:26 +0300

    Ref/null option for fields (#254)
    * add three options for product list filters
    
    * remove loggings

--- app/schemas/uploads/product_compilation_filters.py
+++ app/schemas/uploads/product_compilation_filters.py
@@ -2,9 +2,11 @@ from __future__ import annotations
 
 from typing import List, Optional
 
+from enums import ProductFilterValuesEnum
+
 from ..schema import ApplicationSchema
 
 
 class ProductListFiltersUpload(ApplicationSchema):
     category_ids: Optional[List[int]]
-    on_sale: Optional[bool]
+    on_sale: Optional[ProductFilterValuesEnum] = ProductFilterValuesEnum.ALL
--- app/schemas/uploads/sort_filter_products.py
+++ app/schemas/uploads/sort_filter_products.py
@@ -2,10 +2,12 @@ from __future__ import annotations
 
 from typing import Optional
 
+from enums import ProductFilterValuesEnum
+
 from ..schema import ApplicationSchema
 
 
 class SupplierFilterProductListUpload(ApplicationSchema):
     category_ids: Optional[list[int]]
-    on_sale: Optional[bool]
-    is_active: Optional[bool]
+    on_sale: Optional[ProductFilterValuesEnum]
+    is_active: Optional[ProductFilterValuesEnum]
