083fa9bbbb76b702b0e1fce425eac6591b7653d7
Pavel <107579370+psnewbee@users.noreply.github.com>
2023-12-07 15:28:46 +0300

    Feat/product thumbnails (#289)
    * feat: Add json field in ProductImageModel for thumbnails, update population, schemas
    
    * feat: Add thumbnails upload for product, update thumbnail related methods
    
    * fix: Add brand_id to ProductUpload schema
    
    * fix: Fixed orthographic mistake of get_squared_image_thumbnail_urls method

--- app/schemas/product_image.py
+++ app/schemas/product_image.py
@@ -7,8 +7,11 @@ from .core import ORMSchema
 if TYPE_CHECKING:
     from .product import Product
 
+from typing_ import DictStrAny
+
 
 class ProductImage(ORMSchema):
     image_url: str
     order: int
     product: Optional[Product] = None
+    thumbnail_urls: Optional[DictStrAny] = None
