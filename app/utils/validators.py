1a699e12130935411195a1fe250e827efbde0907
Alexander <110970315+lexsorokin@users.noreply.github.com>
2023-12-19 15:31:50 +0300

    image square validation (#302)
    * image square validation
    
    * merge validators
    
    * delete imports
    
    * change to context manager

--- app/utils/validators.py
+++ app/utils/validators.py
@@ -1,11 +1,12 @@
 from fastapi.datastructures import UploadFile
+from PIL import Image
 from starlette import status
 
 from core.exceptions import UnprocessableEntityException
 from core.settings import upload_file_settings
 
 
-def validate_image_size(
+def validate_image(
     file: UploadFile, max_size: int = upload_file_settings.FILE_SIZE_LIMIT_MB * 1024 * 1024
 ) -> None:
     if file.size > max_size:
@@ -13,3 +14,10 @@ def validate_image_size(
             status_code=status.HTTP_413_REQUEST_ENTITY_TOO_LARGE,
             detail=f"File size is too big. Limit is {upload_file_settings.FILE_SIZE_LIMIT_MB}mb",
         )
+
+    with Image.open(file.file) as img:
+        if img.width != img.height:
+            raise UnprocessableEntityException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail=f"Image should be a square, got ({img.width}x{img.height})",
+            )
