4b5c32cf0fabdb0c28fc06f94b16df0d0cbaaba8
Andrew <drew.yatskevich@gmail.com>
2023-12-20 15:13:49 +0300

    Return all parents with only children that present in supplier products

--- app/api/routers/suppliers/categories.py
+++ app/api/routers/suppliers/categories.py
@@ -9,7 +9,7 @@ from starlette import status
 
 from core.depends import DatabaseSession, SupplierAuthorization, supplier
 from orm import CategoryModel, ProductModel
-from schemas import ApplicationResponse, CategoryDetail
+from schemas import ApplicationResponse, CategoryParent
 from typing_ import RouteReturnT
 
 router = APIRouter(dependencies=[Depends(supplier)])
@@ -19,30 +19,31 @@ async def get_supplier_categories_core(
     supplier_id: int,
     session: AsyncSession,
 ) -> Sequence[CategoryModel]:
-    category_alias = aliased(CategoryModel)
+    category_model_children = aliased(CategoryModel)
+
     categories: Sequence[CategoryModel] = (
         (
             await session.execute(
                 select(CategoryModel)
-                .where(
-                    ProductModel.supplier_id == supplier_id,
+                .join(CategoryModel.products.and_(ProductModel.supplier_id == supplier_id))
+                .outerjoin(
+                    CategoryModel.children.of_type(category_model_children).and_(
+                        category_model_children.id == ProductModel.category_id
+                    )
                 )
-                .join(CategoryModel.products)
-                .outerjoin(CategoryModel.parent.of_type(category_alias))
             )
         )
         .scalars()
         .unique()
         .all()
     )
-
     return categories
 
 
 @router.get(
     path="",
     summary="WORKS: Get all categories from supplier products",
-    response_model=ApplicationResponse[List[CategoryDetail]],
+    response_model=ApplicationResponse[List[CategoryParent]],
     status_code=status.HTTP_200_OK,
 )
 async def get_supplier_categories(
