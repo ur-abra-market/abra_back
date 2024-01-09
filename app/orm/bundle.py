eaaec8d6ed8eb6a683b91d625a4ba0781928a47c
oleh <mefouiny@gmail.com>
2024-01-09 04:25:24 +0200

    add hybrid_property for pickable bundle variations

--- app/orm/bundle.py
+++ app/orm/bundle.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING, List, Optional
 
+from sqlalchemy.ext.hybrid import hybrid_property
 from sqlalchemy.orm import Mapped, relationship
 
 from .core import ORMModel, mixins
@@ -11,14 +12,26 @@ if TYPE_CHECKING:
     from .bundle_price import BundlePriceModel
     from .bundle_product_variation_value import BundleProductVariationValueModel
     from .product import ProductModel
+    from .variation_value_to_product import VariationValueToProductModel
 
 
 class BundleModel(mixins.ProductIDMixin, ORMModel):
     prices: Mapped[Optional[List[BundlePriceModel]]] = relationship(back_populates="bundle")
     product: Mapped[Optional[ProductModel]] = relationship(back_populates="bundles")
     variation_values: Mapped[Optional[List[BundlableVariationValueModel]]] = relationship(
-        back_populates="bundle"
+        back_populates="bundle", lazy="selectin"
     )
     variations: Mapped[Optional[List[BundleProductVariationValueModel]]] = relationship(
         back_populates="bundle"
     )
+
+    @hybrid_property
+    def pickable_variations(self) -> Optional[List[VariationValueToProductModel]]:
+        if not self.variation_values:
+            return None
+        bundlable_type = self.variation_values[0].product_variation.variation.type.id
+        return [
+            variation
+            for variation in self.product.product_variations
+            if not variation.variation.type.id == bundlable_type
+        ]
--- app/schemas/bundle.py
+++ app/schemas/bundle.py
@@ -8,9 +8,12 @@ if TYPE_CHECKING:
     from .bundlable_variation_value import BundlableVariationValue
     from .bundle_price import BundlePrice
     from .product import Product
+    from .variation_value_to_product import VariationValueToProduct
 
 
 class Bundle(ORMSchema):
     prices: Optional[List[BundlePrice]] = None
     product: Optional[Product] = None
     variation_values: Optional[List[BundlableVariationValue]] = None
+
+    pickable_variations: Optional[List[VariationValueToProduct]] = None
