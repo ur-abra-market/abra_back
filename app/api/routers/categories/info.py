5c7b771b9374872350273088fe2ab81953c388a0
Komanok-dev <s.komanok@gmail.com>
2024-02-01 11:54:32 +0000

    add/endpoint_properties_by_category_for_suppliers

--- app/api/routers/categories/info.py
+++ app/api/routers/categories/info.py
@@ -12,49 +12,45 @@ from core.app import crud
 from core.depends import DatabaseSession, supplier
 from orm import (
     CategoryModel,
-    CategoryToPropertyTypeModel,
     CategoryToVariationTypeModel,
-    PropertyValueModel,
+    PropertyTypeModel,
     VariationTypeModel,
     VariationValueModel,
 )
-from schemas import ApplicationResponse, Category, PropertyValue, VariationValue
+from schemas import ApplicationResponse, Category, PropertyType, VariationValue
 from typing_ import RouteReturnT
 
 router = APIRouter()
 
 
-async def get_category_properties_core(
+async def get_product_category_properties_core(
     session: AsyncSession, category_id: int
-) -> List[PropertyValue]:
-    return await crud.categories_property_values.select.many(
-        Where(CategoryToPropertyTypeModel.category_id == category_id),
-        SelectFrom(
-            join(
-                PropertyValueModel,
-                CategoryToPropertyTypeModel,
-                PropertyValueModel.property_type_id
-                == CategoryToPropertyTypeModel.property_type_id,
-            )
-        ),
-        session=session,
+) -> List[PropertyTypeModel]:
+    query = (
+        select(PropertyTypeModel)
+        .join(CategoryModel.properties)
+        .where(CategoryModel.id == category_id)
+        .options(selectinload(PropertyTypeModel.values))
     )
+    result = await session.execute(query)
+    return result.scalars().all()
 
 
 @router.get(
     path="/{category_id}/properties",
-    dependencies=[Depends(supplier)],
-    summary="WORKS: Get all properties names and values by category_id.",
-    response_model=ApplicationResponse[List[PropertyValue]],
+    summary="WORKS: Get all property names and values by category_id.",
+    response_model=ApplicationResponse[List[PropertyType]],
     status_code=status.HTTP_200_OK,
 )
-async def get_category_properties(
+async def get_product_category_properties(
     session: DatabaseSession,
     category_id: int = Path(...),
 ) -> RouteReturnT:
     return {
         "ok": True,
-        "result": await get_category_properties_core(session=session, category_id=category_id),
+        "result": await get_product_category_properties_core(
+            session=session, category_id=category_id
+        ),
     }
 
 
--- app/api/routers/suppliers/products.py
+++ app/api/routers/suppliers/products.py
@@ -24,7 +24,6 @@ from orm import (
     ProductModel,
     ProductPriceModel,
     ProductVariationPriceModel,
-    PropertyTypeModel,
     PropertyValueToProductModel,
     SupplierModel,
     VariationValueModel,
@@ -35,7 +34,6 @@ from schemas import (
     Product,
     ProductImage,
     ProductList,
-    PropertyType,
     VariationValueToProduct,
 )
 from schemas.uploads import (
@@ -643,35 +641,4 @@ async def delete_product_image(
     return {
         "ok": True,
         "result": True,
-    }
-
-
-async def get_product_category_properties_core(
-    session: AsyncSession, category_id: int
-) -> List[PropertyTypeModel]:
-    query = (
-        select(PropertyTypeModel)
-        .join(CategoryModel.properties)
-        .where(CategoryModel.id == category_id)
-    )
-    result = await session.execute(query)
-    return result.scalars().all()
-
-
-@router.get(
-    path="/categories/{category_id}/properties",
-    dependencies=[Depends(supplier)],
-    summary="WORKS: Get all properties by category_id.",
-    response_model=ApplicationResponse[List[PropertyType]],
-    status_code=status.HTTP_200_OK,
-)
-async def get_product_category_properties(
-    session: DatabaseSession,
-    category_id: int = Path(...),
-) -> RouteReturnT:
-    return {
-        "ok": True,
-        "result": await get_product_category_properties_core(
-            session=session, category_id=category_id
-        ),
     }
