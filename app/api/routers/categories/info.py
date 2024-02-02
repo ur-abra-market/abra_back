89c9a5a9fee82e94da3a03bab18d0474fa363534
Komanok-dev <110103059+Komanok-dev@users.noreply.github.com>
2024-02-02 12:30:21 +0000

    add/endpoint_properties_by_category_for_suppliers (#338)
    * add/endpoint_properties_by_category_for_suppliers
    
    * Revert "add/endpoint_properties_by_category_for_suppliers"
    
    This reverts commit 089115ede23e0c25407d802c17b0e3c788105007.
    
    * add/endpoint_properties_by_category_for_suppliers
    
    * add/endpoint_properties_by_category_for_suppliers
    
    * query improve
    
    * query improve

--- app/api/routers/categories/info.py
+++ app/api/routers/categories/info.py
@@ -5,56 +5,52 @@ from fastapi import APIRouter
 from fastapi.param_functions import Depends, Path
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy.orm import join, selectinload
+from sqlalchemy.orm import join, joinedload, selectinload
 from starlette import status
 
 from core.app import crud
 from core.depends import DatabaseSession, supplier
 from orm import (
     CategoryModel,
-    CategoryToPropertyTypeModel,
     CategoryToVariationTypeModel,
-    PropertyValueModel,
+    PropertyTypeModel,
     VariationTypeModel,
     VariationValueModel,
 )
-from schemas import ApplicationResponse, Category, PropertyValue, VariationValue
+from schemas import ApplicationResponse, Category, PropertyType, VariationValue
 from typing_ import RouteReturnT
 
 router = APIRouter()
 
 
-async def get_category_properties_core(
+async def get_product_category_properties_core(
     session: AsyncSession, category_id: int
-) -> List[PropertyValue]:
-    return await crud.categories_property_values.select.many(
-        Where(CategoryToPropertyTypeModel.category_id == category_id),
-        SelectFrom(
-            join(
-                PropertyValueModel,
-                CategoryToPropertyTypeModel,
-                PropertyValueModel.property_type_id
-                == CategoryToPropertyTypeModel.property_type_id,
-            )
-        ),
-        session=session,
+) -> List[PropertyTypeModel]:
+    query = (
+        select(PropertyTypeModel)
+        .join(PropertyTypeModel.category)
+        .where(CategoryModel.id == category_id)
+        .options(joinedload(PropertyTypeModel.values))
     )
+    result = await session.execute(query)
+    return result.unique().scalars().all()
 
 
 @router.get(
     path="/{category_id}/properties",
-    dependencies=[Depends(supplier)],
-    summary="WORKS: Get all variation names and values by category_id.",
-    response_model=ApplicationResponse[List[PropertyValue]],
+    summary="WORKS: Get all property names and values by category_id.",
+    response_model=ApplicationResponse[List[PropertyType]],
     status_code=status.HTTP_200_OK,
 )
-async def get_category_properties(
+async def get_product_category_properties(
     session: DatabaseSession,
     category_id: int = Path(...),
 ) -> RouteReturnT:
     return {
         "ok": True,
-        "result": await get_category_properties_core(session=session, category_id=category_id),
+        "result": await get_product_category_properties_core(
+            session=session, category_id=category_id
+        ),
     }
 
 
