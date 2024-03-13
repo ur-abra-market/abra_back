5644e74feb1321f417eeef61be488c1c46646c47
Sergey Dmitriev <83238503+ardash-ds@users.noreply.github.com>
2024-03-13 18:27:03 +0300

    add status_order_completed (#362)
    * add status_order_completed
    
    * ref order comlete

--- app/api/routers/suppliers/orders.py
+++ app/api/routers/suppliers/orders.py
@@ -99,3 +99,27 @@ async def change_order_status(
         "ok": True,
         "result": True,
     }
+
+
+@router.put(
+    path="/{order_id}/complete",
+    summary="WORKS: changes order status to complete",
+    response_model=ApplicationResponse[bool],
+    status_code=status.HTTP_200_OK,
+)
+async def compete_order_status(
+    user: SupplierAuthorization,
+    session: DatabaseSession,
+    order_id: int = Path(...),
+) -> RouteReturnT:
+    await change_order_status_core(
+        session=session,
+        order_id=order_id,
+        supplier_id=user.supplier.id,
+        status_data=OrderStatusEnum.COMPLETED,
+    )
+
+    return {
+        "ok": True,
+        "result": True,
+    }
